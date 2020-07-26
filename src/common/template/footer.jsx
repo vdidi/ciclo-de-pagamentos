b376745bf87e8ed18c61359fd96d5dc70b7fea5b
vdidi <vitor_hugo_di@hotmail.com>
2020-07-26 14:11:42 -0300

    adiciona footer

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default props => <footer className='main-footer'>
+    <strong>
+      Copyright &copy; 2020
+      <a href="https://github.com/vdidi"> Vitor Hugo Ramos</a>.
+    </strong>
+  </footer>
