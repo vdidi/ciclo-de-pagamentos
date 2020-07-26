4d875cd6130c6faf8808df5d60d6b17e67ac30b9
vdidi <vitor_hugo_di@hotmail.com>
2020-07-26 11:33:10 -0300

    adiciona componente de menu tree

--- src/common/template/menu.jsx
+++ src/common/template/menu.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import MenuItem from './menuItem'
+import MenuTree from './menuTree'
+
 export default props => ( 
   <ul className='sidebar-menu'>
     <MenuItem path='#' label='Dashaboard' icon='dashboard' />
+    <MenuTree label='Cadastro' icon='edit'>
+      <MenuItem path='#billingCycles' 
+        label='Cliclos de Pagamento' icon='usd' />
+    </MenuTree>
   </ul>
 )
